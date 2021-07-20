f702999951396e83be6b1316f923250f0c1a8734
Anthony José <anthonyj2017@icloud.com>
2021-07-20 19:47:49 -0300

    created project and delete storyboards to create ViewCode

---
+++ Company/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Company
+//
+//  Created by Anthony on 20/07/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    private lazy var imageView: UIImageView = {return UIImageView(frame: .zero)}()
+    private lazy var loginLbl: UILabel = { return UILabel(frame: .zero)}()
+    private lazy var loginTextField = { return UITextField(frame: .zero)}()
+    private lazy var senhaLbl = { return UILabel(frame: .zero)}()
+    private lazy var senhaTextField = { return UITextField(frame: .zero)}()
+    private lazy var enterButton = { return UIButton(frame: .zero)}
+}
